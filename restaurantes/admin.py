f4ba253760c9494f397248de6e067bd45782e9e8
ogcoyote83-hash <ogcoyote.83@gmail.com>
2026-09-20 19:16:31 -0300

    retornando galeria de img

--- restaurantes/admin.py
+++ restaurantes/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 from .models import Categoria, Restaurante, Avaliacao, FotoRestaurante
 
+# Cria a secção de galeria de fotos para aparecer dentro do restaurante
+class FotoRestauranteInline(admin.TabularInline):
+    model = FotoRestaurante
+    extra = 1  # Mostra um espaço vazio por defeito para adicionar nova foto
+
 class RestauranteAdmin(admin.ModelAdmin):
-    # O que aparece como colunas na lista principal do painel
+    # O que aparece como colunas na lista principal
     list_display = ('nome', 'categoria', 'hora_abertura', 'hora_fecho')
     
-    # Cria um menu lateral para filtrar facilmente
+    # Cria um menu lateral para filtrar
     list_filter = ('categoria',)
     
-    # Adiciona uma barra de pesquisa
+    # Adiciona a barra de pesquisa
     search_fields = ('nome', 'endereco')
+    
+    # Injeta a secção de fotografias no fundo da página de edição
+    inlines = [FotoRestauranteInline]
 
-# Registar os modelos para aparecerem no Django Admin
+# Regista os modelos no painel
 admin.site.register(Categoria)
 admin.site.register(Restaurante, RestauranteAdmin)
 admin.site.register(Avaliacao)
-admin.site.register(FotoRestaurante)
